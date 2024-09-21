ada21a512062a5cb2e7beaddecac581fbf73790e
Christian Schneiderwind <christian.schneiderwind@gmx.de>
2024-09-22 00:24:16 +0200

    Gogogo!

--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,8 @@
 
 
 
+use std::f32::INFINITY;
+
 use biquad::Coefficients;
 use slint::{ComponentHandle, Model, Weak};
 use crate::ui::*; //{EQManagerUI,TestWindow, EqFilter, EQGraphManager};
@@ -38,6 +40,9 @@ impl AppHandler {
 }  
 
 
+
+
+
 pub struct EqControl {
     
 }
@@ -46,7 +51,6 @@ impl EqControl {
     
     pub fn init(&self, handle: &TestWindow) {
         let eq_manager = handle.global::<EQManagerUI>();
-
         eq_manager.set_test(32.0);
         eq_manager.on_set_filter_frequency({
             let ww = handle.as_weak();
@@ -58,13 +62,94 @@ impl EqControl {
                 filters.set_row_data(id as usize, filter);
         }}
         );
-        // eq_manager.on_update_freq(|id, freq| {
-        //     let o = 1;
-        //     let o2 = 2;
-        //     let handle_weak = handle.as_weak();
-        //     move || {do_smthing(&handle_weak)};
+        eq_manager.on_set_filter_gain({
+            let ww = handle.as_weak();
+            move |id, gain| { 
+                let wu = ww.upgrade().unwrap();
+                let filters  = wu.global::<EQManagerUI>().get_eq_filters();
+                let mut filter = filters.row_data(id as usize).unwrap();
+                filter.gain = gain;
+                filters.set_row_data(id as usize, filter);
+        }}
+        );
+        eq_manager.on_set_filter_q({
+            let ww = handle.as_weak();
+            move |id, q| { 
+                let wu = ww.upgrade().unwrap();
+                let filters  = wu.global::<EQManagerUI>().get_eq_filters();
+                let mut filter = filters.row_data(id as usize).unwrap();
+                filter.q = q;
+                filters.set_row_data(id as usize, filter);
+        }}
+        );
+        eq_manager.on_set_filter_type({
+            let ww = handle.as_weak();
+            move |id, filter_type| { 
+                let wu = ww.upgrade().unwrap();
+                let filters  = wu.global::<EQManagerUI>().get_eq_filters();
+                let mut filter = filters.row_data(id as usize).unwrap();
+                filter.filter_type = filter_type;
+                filters.set_row_data(id as usize, filter);
+        }}
+        );
+        eq_manager.on_set_filter_enabled({
+            let ww = handle.as_weak();
+            move |id, enabled| { 
+                let wu = ww.upgrade().unwrap();
+                let filters  = wu.global::<EQManagerUI>().get_eq_filters();
+                let mut filter = filters.row_data(id as usize).unwrap();
+                filter.enabled = enabled;
+                filters.set_row_data(id as usize, filter);
+        }}
+        );
+
+        // nearest neighbor
+        eq_manager.on_select_nearest_draggable({
+            let ww = handle.as_weak();
+            move|x,y| {
+                let ui = ww.upgrade().unwrap();
+                let filters = ui.global::<EQManagerUI>().get_eq_filters();
+        
+                let min_val: f32 = INFINITY;
+                let mut sel: i32 = -1;
+                let min_val: f32 = filters.iter().enumerate().fold(INFINITY, |min, (n, filter)| { 
+                    let dist: f32 = ((filter.circle_x - x).powi(2) + (filter.circle_y - y).powi(2)).sqrt();
+                    if dist < min && dist < 100.0{
+                        sel = n as i32; dist
+                    } else {
+                        min
+                    }
+        
+                });
+                // if sel != -1 {
+                    ui.global::<EQManagerUI>().set_selected_filter(sel as i32);
+                // }
+            }
+        });
+        // eq_manager.on_select_nearest_draggable2({
+        //     let ww = handle.as_weak();
+        //     move |x,y| {
+        //         let filters = ww.upgrade().unwrap().global::<EQManagerUI>().get_eq_filters();
+        //         let id_selected = 
+        //             filters.iter().enumerate().fold(None, |acc, (i, filter)| {
+        //                 let x1 = filter.frequency;
+        //                 let y1 = filter.gain;
+        //                 let x2 = x;
+        //                 let y2 = y;
+        //                 let dist = (x2 - x1).powi(2) + (y2 - y1).powi(2);
+        //                 match acc {
+        //                     None => Some((i, dist)),
+        //                     Some((_, min_dist)) => {
+        //                         if dist < min_dist {
+        //                             Some((i, dist))
+        //                         } else {
+        //                             acc
+        //                         }
+        //                     }
+        //                 }
+        //         });
+        //     }
         // });
-    
     
     }
 }
